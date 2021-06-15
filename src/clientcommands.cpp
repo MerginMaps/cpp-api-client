d2d1f2718c5b9fb43c7e6c764a5f17a9646300c6
Martin Dobias <wonder.sk@gmail.com>
2021-06-15 14:02:09 +0200

    Fix license in source code files (fixes #7)

--- src/clientcommands.cpp
+++ src/clientcommands.cpp
@@ -1,11 +1,8 @@
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ * mergin-cpp-client
+ * Copyright (C) 2021 Lutra Consulting
+ * License: MIT
+ */
 
 #include "clientcommands.h"
 #include <QEventLoop>
